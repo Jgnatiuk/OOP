a0c5c55320a44e3fe7b80640e34a82edc027b8b6
Jgnatiuk <jag166@zips.uakron.edu>
2019-05-04 03:24:57 -0400

    MILESTONE 4: moved event processing loop outside of main, extended functionality

--- sfml_snake/main.cpp
+++ sfml_snake/main.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <time.h>
+#include <iostream>
 #include "snakeObjects.h"
 using namespace sf;
 
@@ -7,6 +8,7 @@ int N = 30, M = 20;
 int size = 16;
 int w = size * N;
 int h = size * M;
+bool isDead = false;
 
 int dir, num = 4;
 
@@ -16,115 +18,117 @@ Snake s[100];
 // Declare fruit object
 Fruit f;
 
- void die()
-{
-    for (int i = num; i > 0; --i)
-    {
-        num -= 1;
-    }
-}
-
-///// ADD REPLAY BUTTON IN DIE FUNCTION /////
-
-void Tick(bool &death)
+struct Map
 {
 
-  for (int i = num; i > 0; --i) {
-    s[i].x = s[i - 1].x;
-    s[i].y = s[i - 1].y;
-  }
-
-  if (dir == 0)
-    s[0].y += 1;
-  if (dir == 1)
-    s[0].x -= 1;
-  if (dir == 2)
-    s[0].x += 1;
-  if (dir == 3)
-    s[0].y -= 1;
-
-  if ((s[0].x == f.x) && (s[0].y == f.y)) {
-    num++;
-    f.x = rand() % N;
-    f.y = rand() % M;
-    f.fruitTimer = 0;
-  }
-
-  if (s[0].x > N)
-  {
-    death = true;
-    die();
-  }
-  if (s[0].x < 0)
-  {
-    death = true;
-    die();
-  }
-  if (s[0].y > M)
-  {
-    death = true;
-    die();
-  }
-  if (s[0].y < 0)
-  {
-    death = true;
-    die();
-  }
-
-  for (int i = 1; i < num; i++)
-    if (s[0].x == s[i].x && s[0].y == s[i].y)
-      num = i;
-
-  // Increment fruit timer
-  f.fruitTimer += 1;
-
-  // If fruit has not been obtained in 5 seconds, regenerate fruit
-  if(f.fruitTimer == 50)
-  {
-    f.x = rand() % N;
-    f.y = rand() % M;
-    f.fruitTimer = 0;
-  }
-}
-
-int
-main()
-{
-  srand(time(0));
-
-  RenderWindow window(VideoMode(w, h), "Snake Game!");
 
   Texture t1, t2, t3, gameOver;
-  t1.loadFromFile("images/white.png");
-  t2.loadFromFile("images/red.png");
-  t3.loadFromFile("images/green.png");
-  gameOver.loadFromFile("images/GAMEOVER.png");
+  Sprite sprite1, sprite2, sprite3, gameOverSprite;
+  RenderWindow window;
 
-  Sprite sprite1(t1);
-  Sprite sprite2(t2);
-  Sprite sprite3(t3);
-  Sprite gameOverSprite(gameOver);
+  Map()
+   : window(VideoMode(w, h), "Snake Game!")
+  {}
 
+  void loadAssets()
+  {
+    t1.loadFromFile("images/white.png");
+    t2.loadFromFile("images/red.png");
+    t3.loadFromFile("images/green.png");
+    gameOver.loadFromFile("images/GAMEOVER.png");
 
-  Clock clock;
-  float timer = 0, delay = 0.1;
+    sprite1.setTexture(t1);
+    sprite2.setTexture(t2);
+    sprite3.setTexture(t3);
 
-  f.x = 10;
-  f.y = 10;
+    gameOverSprite.setTexture(gameOver);
+  }
 
-  bool isDead = false;
+  void buildMap()
+  {
+    ////// draw  ///////
+    window.clear();
 
-  while (window.isOpen()) {
-    float time = clock.getElapsedTime().asSeconds();
-    clock.restart();
-    timer += time;
+    ///// draw playing area /////
+    for (int i = 0; i < N; i++)
+      for (int j = 0; j < M; j++) {
+        sprite1.setPosition(i * size, j * size);
+        window.draw(sprite1);
+      }
 
-    Event e;
-    while (window.pollEvent(e)) {
-      if (e.type == Event::Closed)
-        window.close();
+    ///// draw snake /////
+    for (int i = 0; i < num; i++)
+    {
+      sprite2.setPosition(s[i].x * size, s[i].y * size);
+      window.draw(sprite2);
+      std::cout << s[i].x << " " << s[i].y << std::endl;
     }
 
+    ///// draw fruit /////
+    sprite3.setPosition(f.x * size, f.y * size);
+    window.draw(sprite3);
+
+    window.display();
+  }
+
+  void death()
+  {
+    ////// draw  ///////
+    window.clear();
+
+    ///// draw playing area /////
+    for (int i = 0; i < N; i++)
+      for (int j = 0; j < M; j++)
+      {
+        sprite1.setPosition(i * size, j * size);
+        window.draw(sprite1);
+      }
+
+      ///// draw fruit /////
+    sprite3.setPosition(f.x * size, f.y * size);
+    window.draw(sprite3);
+
+      ///// draw game over button /////
+    gameOverSprite.setPosition(w/4, h/3);
+    window.draw(gameOverSprite);
+
+    window.display();
+  }
+  void pollWindowEvent()
+  {
+
+    Event e;
+    while (window.pollEvent(e))
+    {
+        if (e.type == Event::Closed)
+            window.close();
+
+        if (e.type == Event::MouseButtonPressed)
+        {
+            if(e.mouseButton.button == Mouse::Left)
+            {
+                isDead = false;
+                num = 4;
+                dir = 0;
+                for(int j = 0; j < num; j++)
+                {
+                    s[j].x = 0;
+                    s[j].y = 0;
+                }
+            }
+
+        }
+    }
+  }
+
+
+
+};
+
+
+
+int getDirection()
+{
     if (Keyboard::isKeyPressed(Keyboard::Left))
     {
       // if going right, can't go left, keep going right
@@ -153,36 +157,118 @@ main()
       // else go down
       else dir = 0;
     }
+};
+void Tick(bool &death, int direction)
+{
 
-    if (timer > delay) {
-      timer = 0;
-      Tick(isDead);
+  for (int i = num; i > 0; --i)
+  {
+    s[i].x = s[i - 1].x;
+    s[i].y = s[i - 1].y;
+  }
+
+  if (dir == 0)
+    s[0].y += 1;
+  if (dir == 1)
+    s[0].x -= 1;
+  if (dir == 2)
+    s[0].x += 1;
+  if (dir == 3)
+    s[0].y -= 1;
+
+  if ((s[0].x == f.x) && (s[0].y == f.y))
+  {
+    num++;
+    f.x = rand() % N;
+    f.y = rand() % M;
+    f.fruitTimer = 0;
+  }
+
+  if (s[0].x > N)
+  {
+
+    death = true;
+  }
+  if (s[0].x < 0)
+  {
+    death = true;
+  }
+  if (s[0].y > M)
+  {
+    death = true;
+  }
+  if (s[0].y < 0)
+  {
+    death = true;
+  }
+
+  for (int i = 1; i < num; i++)
+    if (s[0].x == s[i].x && s[0].y == s[i].y)
+      death = true;
+
+
+
+  // Increment fruit timer
+  f.fruitTimer += 1;
+
+  // If fruit has not been obtained in 5 seconds, regenerate fruit
+  if(f.fruitTimer == 50)
+  {
+    f.x = rand() % N;
+    f.y = rand() % M;
+    f.fruitTimer = 0;
+  }
+
+}
+
+void reset(Map &map, bool&isDead)
+{
+    isDead = false;
+
+}
+
+int
+main()
+{
+  srand(time(0));
+
+  Map gameGrid;
+
+  gameGrid.loadAssets();
+  gameGrid.buildMap();
+
+
+  Clock clock;
+  float timer = 0, delay = 0.1;
+
+  f.x = 10;
+  f.y = 10;
+
+  while (gameGrid.window.isOpen())
+  {
+    float time = clock.getElapsedTime().asSeconds();
+    clock.restart();
+    timer += time;
+
+    gameGrid.pollWindowEvent();
+
+    if(isDead == false)
+    {
+
+        if (timer > delay)
+        {
+            timer = 0;
+            Tick(isDead, getDirection());
+        }
+
+        gameGrid.buildMap();
+
     }
 
-    ////// draw  ///////
-    window.clear();
+    else
 
-    ///// draw playing area /////
-    for (int i = 0; i < N; i++)
-      for (int j = 0; j < M; j++) {
-        sprite1.setPosition(i * size, j * size);
-        window.draw(sprite1);
-      }
+        gameGrid.death();
 
-    ///// draw snake /////
-    for (int i = 0; i < num; i++) {
-      sprite2.setPosition(s[i].x * size, s[i].y * size);
-      window.draw(sprite2);
-    }
-
-    ///// draw fruit /////
-    sprite3.setPosition(f.x * size, f.y * size);
-    window.draw(sprite3);
-
-    gameOverSprite.setPosition(w/4, h/3);
-    if(isDead == true) window.draw(gameOverSprite);
-
-    window.display();
   }
 
   return 0;
